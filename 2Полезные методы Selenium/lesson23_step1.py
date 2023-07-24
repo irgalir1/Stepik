a4f7dc3ee0df42a038b7eaac78bf38f7bd599e57
unknown <C:\Users\UserAccount\AppData\Roaming\The Bat!>
2023-07-24 18:50:54 +0300

    2.3 Работа с окнами

---
+++ 2Полезные методы Selenium/lesson23_step1.py
@@ -0,0 +1,37 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+ 
+link = "http://suninjuly.github.io/alert_accept.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+button.click()
+
+
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+x_element = browser.find_element(By.ID, "input_value")
+x = x_element.text
+y = calc(x) 
+
+
+input1 = browser.find_element(By.ID, "answer")
+input1.send_keys(y)
+
+button = browser.find_element(By.CSS_SELECTOR, "#solve")
+button.click()
+
+ 
+
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+time.sleep(20)
+    # закрываем браузер после всех манипуляций
+browser.quit()
---
+++ 2Полезные методы Selenium/lesson23_step2.py
@@ -0,0 +1,30 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+ 
+link = " http://suninjuly.github.io/redirect_accept.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+alert = browser.switch_to.alert
+alert_text = alert.text
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+x = x_element.text
+y = calc(x) 
+input1 = browser.find_element(By.ID, "answer")
+input1.send_keys(y)
+
+button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+button.click()
+ 
+
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+time.sleep(10)
+    # закрываем браузер после всех манипуляций
+browser.quit()
